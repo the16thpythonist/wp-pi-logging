2e75213848281b098cebcc75c7921be363acae52
the16thpythonist <jonseb1998@gmail.com>
2018-06-28 13:01:46 +0200

    0.0.0.5

--- src/Log/LogPostRegistration.php
+++ src/Log/LogPostRegistration.php
@@ -184,7 +184,7 @@ class LogPostRegistration
             $count = 0;
             foreach ($log as $message):
             ?>
-                <?php $line = $count + ' ' * (5 - strlen(strval($count))); ?>
+                <?php $line = $count . str_repeat(' ', 5 - strlen(strval($count))); ?>
                 <p><span style="color: dimgrey; font-size: 80%"><?php echo $line; ?></span><?php echo $message; ?></p>
                 <?php $count+= 1; ?>
             <?php endforeach; ?>
